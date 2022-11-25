98f67e58a012a1d4fef46f307e402aaac59f1384
Julian Metzger <julianjometzger@gmail.com>
2022-11-25 19:16:08 +0100

    i rewrote the snake game

--- script.js
+++ script.js
@@ -1,109 +1,162 @@
-const start = document.querySelector(".start");
+class Snake {
+  constructor(field, width = 20, height = 20) {
+    this.field = field;
+    this.width = width;
+    this.height = height;
+    this.cords = [
+      {
+        x: 40,
+        y: 0,
+      },
+      {
+        x: 20,
+        y: 0,
+      },
+      {
+        x: 0,
+        y: 0,
+      },
+    ];
+  }
+
+  get headx() {
+    return this.cords[0].x;
+  }
+  get heady() {
+    return this.cords[0].y;
+  }
+
+  move(dirx, diry) {
+    this.dirx = dirx;
+    this.diry = diry;
+    if (this.cords.length - 1) {
+      for (let i = this.cords.length - 1; i > 0; i--) {
+        this.cords[i].x = this.cords[i - 1].x;
+        this.cords[i].y = this.cords[i - 1].y;
+      }
+    }
+    this.cords[0].x += this.width * dirx;
+    this.cords[0].y += this.height * diry;
+
+    //! Zoptymalizuj to!!!!!!!!!
+    if (this.cords[0].x >= this.field.clientWidth) this.cords[0].x = 0;
+    if (this.cords[0].x < 0) this.cords[0].x = this.field.clientWidth;
+    if (this.cords[0].y >= this.field.clientHeight) this.cords[0].y = 0;
+    if (this.cords[0].y < 0) this.cords[0].y = this.field.clientHeight;
+    //!!!!!!!!!!
+
+    if (this.cords.length > 3) {
+      for (let i = 4; i < this.cords.length; i++) {
+        if (
+          this.cords[i].x == this.cords[0].x &&
+          this.cords[i].y == this.cords[0].y
+        ) {
+          this.draw();
+          this.die();
+        }
+      }
+    }
+  }
+  draw() {
+    const context = this.field.getContext("2d");
+    context.clearRect(0, 0, this.field.clientWidth, this.field.clientHeight);
+    context.fillStyle = "green";
+    this.cords.map((e) => context.fillRect(e.x, e.y, this.width, this.height));
+  }
+
+  addElement() {
+    this.cords.push({
+      x: this.cords[this.cords.length - 1].x + this.width * this.dirx,
+      y: this.cords[this.cords.length - 1].y + this.height * this.diry,
+    });
+  }
+
+  die() {
+    this.draw();
+    window.alert("Game Over");
+    this.cords = [
+      {
+        x: 40,
+        y: 0,
+      },
+      {
+        x: 20,
+        y: 0,
+      },
+      {
+        x: 0,
+        y: 0,
+      },
+    ];
+    this.move(1, 0);
+  }
+}
+
+class Apple {
+  constructor(field, score) {
+    this.field = field;
+    this.score = score;
+    this.cords = {
+      x: Math.floor(Math.random() * 21) * 20,
+      y: Math.floor(Math.random() * 21) * 20,
+    };
+  }
+  draw() {
+    const context = this.field.getContext("2d");
+    context.fillStyle = "red";
+    context.fillRect(this.cords.x, this.cords.y, 20, 20);
+  }
+  addPoint() {
+    const x = snake.headx;
+    const y = snake.heady;
+    if (this.cords.x == x && this.cords.y == y) {
+      this.cords.x = Math.floor(Math.random() * 21) * 20;
+      this.cords.y = Math.floor(Math.random() * 21) * 20;
+      points++;
+      score.innerHTML = points;
+      snake.addElement();
+    }
+  }
+}
+
 const field = document.querySelector("canvas");
+const start = document.querySelector(".start");
 const score = document.querySelector("p");
-const ctx = field.getContext("2d");
 
-const width = field.clientWidth / 20,
-  height = field.clientHeight / 20;
-let dirx = 0,
+const snake = new Snake(field);
+const apple = new Apple(field, score);
+
+let dirx = 1,
   diry = 0;
-var cord = [
-  {
-    x: field.clientWidth / 2 - width / 2,
-    y: field.clientWidth / 2 - width / 2,
-  },
-];
-
-let apple = {
-  is: 0,
-  x: Math.round(Math.random() * (field.clientWidth - width)),
-  y: Math.round(Math.random() * (field.clientHeight - height)),
-};
-
 let points = 0;
 
-const init = () => {
-  cord = [
-    {
-      x: field.clientWidth / 2 - width / 2,
-      y: field.clientWidth / 2 - width / 2,
-    },
-  ];
-  const interval = setInterval(draw, 200);
-};
-
-const draw = () => {
-  ctx.clearRect(0, 0, field.clientWidth, field.clientHeight);
-  ctx.fillStyle = "#aaa";
-  cord.map((e) => ctx.fillRect(e.x, e.y, width, height));
-  cord.map((e) => {
-    e.x += width * dirx;
-    e.y += height * diry;
-  });
-
-  //teleportation
-  cord.map((e) => {
-    if (e.x > field.clientWidth - width) e.x = 0;
-    if (e.y > field.clientHeight - height) e.y = 0;
-    if (e.x < 0) e.x = field.clientWidth - width;
-    if (e.y < 0) e.y = field.clientHeight - height;
-  });
-
-  if (
-    cord[0].x >= apple.x - width &&
-    cord[0].x <= apple.x + width &&
-    cord[0].y >= apple.y - height &&
-    cord[0].y <= apple.y + height
-  ) {
-    apple.x = Math.round(Math.random() * (field.clientWidth - width));
-    apple.y = Math.round(Math.random() * (field.clientHeight - height));
-    points++;
-    score.innerHTML = points;
-    addElement();
-    console.log(cord);
+document.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "ArrowUp":
+      if (diry == 1) break;
+      diry = -1;
+      dirx = 0;
+      break;
+    case "ArrowDown":
+      if (diry == -1) break;
+      diry = 1;
+      dirx = 0;
+      break;
+    case "ArrowRight":
+      if (dirx == -1) break;
+      diry = 0;
+      dirx = 1;
+      break;
+    case "ArrowLeft":
+      if (dirx == 1) break;
+      diry = 0;
+      dirx = -1;
   }
-  if (apple.is) {
-    ctx.fillStyle = "rgb(255, 0, 0)";
-    ctx.fillRect(apple.x, apple.y, width, height);
-  }
-  if (!apple.is) {
-    ctx.fillStyle = "rgb(255, 0, 0)";
-    ctx.fillRect(apple.x, apple.y, width, height);
-    apple.is = 1;
-  }
-};
+});
 
-const addElement = () => {
-  cord.push({
-    x: cord[cord.length - 1].x + width * dirx,
-    y: cord[cord.length - 1].y + height * diry,
-  });
-};
-
-window.onload = () => {
-  start.addEventListener("click", init);
-  document.addEventListener("keydown", (e) => {
-    switch (e.key) {
-      case "ArrowRight":
-        if (dirx * -1) break;
-        dirx = 1;
-        diry = 0;
-        break;
-      case "ArrowLeft":
-        if (dirx) break;
-        dirx = -1;
-        diry = 0;
-        break;
-      case "ArrowUp":
-        if (diry) break;
-        dirx = 0;
-        diry = -1;
-        break;
-      case "ArrowDown":
-        if (diry * -1) break;
-        dirx = 0;
-        diry = 1;
-        break;
-    }
-  });
-};
+const interval = setInterval(() => {
+  snake.draw();
+  apple.draw();
+  apple.addPoint();
+  snake.move(dirx, diry);
+}, 100);
